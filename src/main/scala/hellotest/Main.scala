0648e1b00d4a970ce2b36368e19a692a44074de8
rue <rhu1@luc.edu>
2024-09-27 15:48:11 -0500

    reverting code back due to clarifications in office hours

--- src/main/scala/hellotest/Main.scala
+++ src/main/scala/hellotest/Main.scala
@@ -1,6 +1,5 @@
 package hellotest
 
-import scala.io.Source
 import java.util.Scanner
 import org.apache.commons.collections4.queue.CircularFifoQueue
 import scala.collection.mutable
@@ -13,36 +12,26 @@ object Main:
   val WINDOW_SIZE = 1000
 
   @main
-  def run(args: String*): Unit = {
+  ef run(args: String*): Unit = {
     val cloudSize = if (args.length > 0) args(0).toIntOption.getOrElse(CLOUD_SIZE) else CLOUD_SIZE
     val lengthAtLeast = if (args.length > 1) args(1).toIntOption.getOrElse(LENGTH_AT_LEAST) else LENGTH_AT_LEAST
     val windowSize = if (args.length > 2) args(2).toIntOption.getOrElse(WINDOW_SIZE) else WINDOW_SIZE
     println(s"Starting with cloudSize = $cloudSize, lengthAtLeast = $lengthAtLeast, windowSize = $windowSize")
 
-    //SIGPIPE? ?
+    //work on sigpipe 
     sys.addShutdownHook {
       println("Terminating gracefully due to SIGPIPE")
       exit(0)
     }
 
     val wordProcessor = new StreamFrequencySorter(cloudSize, lengthAtLeast, windowSize)
-
     val scanner = new Scanner(System.in)
-    // takes in user input
-    if (args.nonEmpty && args.mkString(" ").startsWith("\"") && args.mkString(" ").endsWith("\"")) {
-      val quotedText = args.mkString(" ").stripPrefix("\"").stripSuffix("\"")
-      println(s"Processing quoted text: $quotedText")
-      
-      quotedText.split("\\s+").foreach { word =>
-        if (word.length >= lengthAtLeast) wordProcessor.processWord(word)
-      }
-    } 
-    //takes in a file 
-    else {
-      println("Processing input from stdin (file or terminal)...")
-      val input = Source.stdin.getLines()
-      input.foreach { word =>
-        if (word.length >= lengthAtLeast) wordProcessor.processWord(word)7
+
+    while (scanner.hasNext()) {
+      val word = scanner.next()
+
+      if (word.nn.length >= lengthAtLeast) { 
+        wordProcessor.processWord(word.nn)
       }
     }
   }
@@ -56,7 +45,7 @@ class StreamFrequencySorter(var cloudSize: Int, var lengthAtLeast: Int, var wind
     wordFrequency(word) = wordFrequency.getOrElse(word, 0) + 1
 
     if (wordQueue.size > windowSize) {
-      val oldestWord = wordQueue.remove().nn
+      val oldestWord = wordQueue.remove().nn // Use .nn here
       wordFrequency(oldestWord) = wordFrequency(oldestWord) - 1
       if (wordFrequency(oldestWord) == 0) {
         wordFrequency.remove(oldestWord)
