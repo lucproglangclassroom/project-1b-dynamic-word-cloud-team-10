817f1a2cd7f1c2a50f12b1881ffe861ae1a48ee9
Areej <areejimran86@gmail.com>
2024-09-28 21:28:06 -0500

    Main is running as expected. Test cases are needed.

--- build.sbt
+++ build.sbt
@@ -6,13 +6,43 @@ scalaVersion := "3.3.3"
 
 scalacOptions += "@.scalacOptions.txt"
 
-libraryDependencies += "org.log4s" %% "log4s" % "1.10.0" 
+libraryDependencies += "org.log4s" %% "log4s" % "1.10.0"
 libraryDependencies += "org.slf4j" % "slf4j-simple" % "1.7.30"
 
 libraryDependencies ++= Seq(
   "org.scalatest"  %% "scalatest"  % "3.2.19"  % Test,
   "com.lihaoyi" %% "mainargs" % "0.6.3",
   "org.apache.commons" % "commons-collections4" % "4.4",
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5",
+  "ch.qos.logback" % "logback-classic" % "1.4.7",
+  "com.github.scopt" %% "scopt" % "4.1.0",
+  "org.log4s" %% "log4s" % "1.10.0",
+  "org.scalacheck" %% "scalacheck" % "1.18.0" % Test
+  //"org.log4s" %% "log4s" % "1.8.2",
+  //"org.slf4j" % "slf4j-simple" % "1.7.30"
+)
+
+enablePlugins(JavaAppPackaging)
+
+name := "hello-scalatest-scala"
+
+version := "0.3"
+
+scalaVersion := "3.3.3"
+
+scalacOptions += "@.scalacOptions.txt"
+
+libraryDependencies += "org.log4s" %% "log4s" % "1.10.0"
+libraryDependencies += "org.slf4j" % "slf4j-simple" % "1.7.30"
+
+libraryDependencies ++= Seq(
+  "org.scalatest"  %% "scalatest"  % "3.2.19"  % Test,
+  "com.lihaoyi" %% "mainargs" % "0.6.3",
+  "org.apache.commons" % "commons-collections4" % "4.4",
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5",
+  "ch.qos.logback" % "logback-classic" % "1.4.7",
+  "com.github.scopt" %% "scopt" % "4.1.0",
+  "org.log4s" %% "log4s" % "1.10.0",
   "org.scalacheck" %% "scalacheck" % "1.18.0" % Test
   //"org.log4s" %% "log4s" % "1.8.2",
   //"org.slf4j" % "slf4j-simple" % "1.7.30"
--- src/main/scala/hellotest/Main.scala
+++ src/main/scala/hellotest/Main.scala
@@ -1,46 +1,87 @@
 package hellotest
 
-import java.util.Scanner
 import org.apache.commons.collections4.queue.CircularFifoQueue
 import scala.collection.mutable
 import scala.language.unsafeNulls
 import scala.sys.exit
+import org.log4s._
+import scala.io.Source
 
-object Main:
-  val CLOUD_SIZE = 10
-  val LENGTH_AT_LEAST = 6
-  val WINDOW_SIZE = 1000
+object Main {
+  private[this] val logger = org.log4s.getLogger
+  var PRINT_COUNTER: Int = 0
 
-  private val logger = org.log4s.getLogger // initialize logger
+  def main(args: Array[String]): Unit = {
+    // Default values for the arguments
+    var cloudSize = 10
+    var kSteps = 6
+    var minFrequency = 1
+    var minLength = 1
+    var windowSize = 1000
 
-  @main
-  def run(args: String*): Unit = {
-    val cloudSize = if (args.length > 0) args(0).toIntOption.getOrElse(CLOUD_SIZE) else CLOUD_SIZE
-    val lengthAtLeast = if (args.length > 1) args(1).toIntOption.getOrElse(LENGTH_AT_LEAST) else LENGTH_AT_LEAST
-    val windowSize = if (args.length > 2) args(2).toIntOption.getOrElse(WINDOW_SIZE) else WINDOW_SIZE
-    println(s"Starting with cloudSize = $cloudSize, lengthAtLeast = $lengthAtLeast, windowSize = $windowSize")
+    // Parse command-line arguments (if provided)
+    if (args.length > 0) cloudSize = args(0).toInt
+    if (args.length > 1) kSteps = args(1).toInt
+    if (args.length > 2) minFrequency = args(2).toInt
+    if (args.length > 3) minLength = args(3).toInt
+    if (args.length > 4) windowSize = args(4).toInt
 
-    logger.debug(f"cloudSize = $cloudSize lengthAtLeast = $lengthAtLeast windowSize = $windowSize")
+    // Log the received arguments
+    logger.debug(s"cloudSize: $cloudSize, kSteps: $kSteps, minFrequency: $minFrequency, minLength: $minLength, windowSize: $windowSize")
 
-    //work on sigpipe 
+    // Setup SIGPIPE handling
     sys.addShutdownHook {
       println("Terminating gracefully due to SIGPIPE")
       exit(0)
     }
 
-    val wordProcessor = new StreamFrequencySorter(cloudSize, lengthAtLeast, windowSize)
-    val scanner = new Scanner(System.in)
+    // Initialize the word processor with a frequency sorter
+    val wordProcessor = new StreamFrequencySorter(cloudSize, minLength, windowSize, minFrequency)
 
-    while (scanner.hasNext()) {
-      val word = scanner.next()
+    // Read words from standard input (can be from a continuous stream)
+    val lines = Source.stdin.getLines
+    val words = lines.flatMap(line => Option(line).map(_.split("(?U)[^\\p{Alpha}0-9']+")).getOrElse(Array.empty[String]))
 
-      if (word.nn.length >= lengthAtLeast) { 
-        wordProcessor.processWord(word.nn)
+    // Process each word
+    words.filter(_ != null).foreach { word =>
+      if (word.length >= minLength) {
+        // Convert word to lowercase for case-insensitive comparison
+        val lowercasedWord = word.toLowerCase
+        wordProcessor.processWord(lowercasedWord)
+
+        // Increment print counter and check if we should print the top words
+        PRINT_COUNTER += 1
+        if (PRINT_COUNTER % kSteps == 0) {
+          printWordCloud(wordProcessor, cloudSize)
+        }
       }
     }
+
+    // Print final word cloud after all input has been processed (EOF)
+    printWordCloud(wordProcessor, cloudSize)
   }
 
-class StreamFrequencySorter(var cloudSize: Int, var lengthAtLeast: Int, var windowSize: Int) {
+  // Helper method to print the top words in the word cloud
+  // Adjusted printWordCloud method to remove unnecessary colons and ensure correct formatting
+  def printWordCloud(wordProcessor: StreamFrequencySorter, cloudSize: Int): Unit = {
+    val topWords = wordProcessor.getTopWords(cloudSize)
+
+    // Print the top words in the desired format: "word: frequency"
+    if (topWords.nonEmpty) {
+      println(topWords.map { case (word, count) => s"$word: $count" }.mkString(" "))
+    }
+
+    Console.flush() // Ensure the output is flushed to the console
+  }
+}
+
+// The StreamFrequencySorter class is complete
+class StreamFrequencySorter(
+                             var cloudSize: Int,
+                             var minLength: Int,
+                             var windowSize: Int,
+                             var minFrequency: Int
+                           ) {
   private val wordFrequency: mutable.Map[String, Int] = mutable.Map()
   private val wordQueue = new CircularFifoQueue[String](windowSize)
 
@@ -48,18 +89,24 @@ class StreamFrequencySorter(var cloudSize: Int, var lengthAtLeast: Int, var wind
     wordQueue.add(word)
     wordFrequency(word) = wordFrequency.getOrElse(word, 0) + 1
 
-    if (wordQueue.size > windowSize) {
-      val oldestWord = wordQueue.remove().nn // Use .nn here
-      wordFrequency(oldestWord) = wordFrequency(oldestWord) - 1
-      if (wordFrequency(oldestWord) == 0) {
-        wordFrequency.remove(oldestWord)
+    // Automatically remove oldest word when queue exceeds capacity
+    if (wordQueue.size == windowSize) {
+      Option(wordQueue.peek()).foreach { oldestWord =>
+        wordFrequency(oldestWord) = wordFrequency(oldestWord) - 1
+        if (wordFrequency(oldestWord) == 0) {
+          wordFrequency.remove(oldestWord)
+        }
       }
     }
+  }
 
-    val sortedByFrequency = wordFrequency.toSeq.sortBy { case (word, count) => (-count, word) }.take(cloudSize)
-
-    if (sortedByFrequency.nonEmpty) {
-      println(sortedByFrequency.map { case (word, count) => s"$word: $count" }.mkString(" "))
-    }
+  def getTopWords(topN: Int): Seq[(String, Int)] = {
+    wordFrequency
+      .filter(_._2 >= minFrequency) // Filter by minimum frequency
+      .toSeq
+      .sortBy { case (_, count) => -count } // Sort by frequency in descending order
+      .take(topN) // Take the top N most frequent words
   }
 }
+
+// Add closing brace for Main object
