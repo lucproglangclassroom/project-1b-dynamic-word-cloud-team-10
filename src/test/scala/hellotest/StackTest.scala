1cadcafc32532d100f58f77791f5ffaa64c65bfb
Katarina Krivokapic <katarina03kk@gmail.com>
2024-09-30 10:28:34 -0500

    fixing test suite

--- src/test/scala/hellotest/StackTest.scala
+++ src/test/scala/hellotest/StackTest.scala
@@ -1,69 +1,42 @@
 package hellotest
 
-// example straight from scalatest.org
-
-import scala.collection.mutable.Stack
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.Suite
-import org.scalatest.matchers.must.Matchers.*
-import java.io.ByteArrayOutputStream
-import java.io.PrintStream
+import org.scalatest.matchers.must.Matchers._
 
-class StackSpec extends AnyFlatSpec with Suite:
+class StreamFrequencySorterSpec extends AnyFlatSpec {
 
-  "A Stack" should "pop values in last-in-first-out order" in:
-    val stack = Stack.empty[Int]
-    stack.push(1)
-    stack.push(2)
-    stack.pop() must equal(2)
-    stack.pop() must equal(1)
-
-  it should "throw NoSuchElementException if an empty stack is popped" in:
-    val emptyStack = Stack.empty[Int]
-    an[NoSuchElementException] must be thrownBy:
-      emptyStack.pop()
-
-end StackSpec
-
-class StreamFrequencySorterSpec extends AnyFlatSpec:
-
-  "StreamFrequencySorter" should "update word frequencies correctly" in:
-    val outputStream = new ByteArrayOutputStream()
-    val printStream = new PrintStream(outputStream)
-    val sorter = new StreamFrequencySorter(3, 4, 5, printStream)
+  "StreamFrequencySorter" should "update word frequencies correctly" in {
+    val sorter = new StreamFrequencySorter(10, 1, 10, 1)
 
     sorter.processWord("hello")
     sorter.processWord("world")
     sorter.processWord("hello")
 
-    val output = outputStream.toString.trim
-    output must include("hello: 2")
-    output must include("world: 1")
+    // Check that the frequencies match the expected values
+    sorter.getTopWords(2) must equal(Seq("hello" -> 2, "world" -> 1))
+  }
 
-  it should "remove old words when exceeding window size" in:
-    val outputStream = new ByteArrayOutputStream()
-    val printStream = new PrintStream(outputStream)
-    val sorter = new StreamFrequencySorter(3, 4, 3, printStream)
+  it should "remove old words when exceeding window size" in {
+    val sorter = new StreamFrequencySorter(10, 1, 3, 1)
 
     sorter.processWord("scala")
     sorter.processWord("java")
     sorter.processWord("kotlin")
-    sorter.processWord("python") // This will push "scala" out of the window
+    sorter.processWord("python") // "scala" should be removed
 
-    val output = outputStream.toString.trim
-    output must not include "scala:"
+    // "scala" should no longer be in the top words due to window size constraints
+    sorter.getTopWords(3).map(_._1) must not contain ("scala")
+  }
 
-  it should "sort words by frequency and alphabetically" in:
-    val outputStream = new ByteArrayOutputStream()
-    val printStream = new PrintStream(outputStream)
-    val sorter = new StreamFrequencySorter(2, 4, 5, printStream)
+  it should "sort words by frequency and alphabetically when frequencies are equal" in {
+    val sorter = new StreamFrequencySorter(10, 1, 10, 1)
 
     sorter.processWord("apple")
     sorter.processWord("banana")
-    sorter.processWord("apple")
+    sorter.processWord("cherry")
+    sorter.processWord("banana")
 
-    val output = outputStream.toString.trim
-    output must include("apple: 2")
-    output must include("banana: 1")
-
-end StreamFrequencySorterSpec
+    // "banana" should come first due to higher frequency, "apple" and "cherry" are alphabetically sorted
+    sorter.getTopWords(2) must equal(Seq("banana" -> 2, "apple" -> 1))
+  }
+}
