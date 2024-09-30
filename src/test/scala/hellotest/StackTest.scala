2258827964979f0349b7c4c07d3c947a4425e7e8
arubsumayli <asumayli@luc.edu>
2024-09-29 20:17:56 -0500

    test cases

--- src/test/scala/hellotest/StackTest.scala
+++ src/test/scala/hellotest/StackTest.scala
@@ -3,10 +3,11 @@ package hellotest
 // example straight from scalatest.org
 
 import scala.collection.mutable.Stack
-
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.Suite
 import org.scalatest.matchers.must.Matchers.*
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
 
 class StackSpec extends AnyFlatSpec with Suite:
 
@@ -23,3 +24,46 @@ class StackSpec extends AnyFlatSpec with Suite:
       emptyStack.pop()
 
 end StackSpec
+
+class StreamFrequencySorterSpec extends AnyFlatSpec:
+
+  "StreamFrequencySorter" should "update word frequencies correctly" in:
+    val outputStream = new ByteArrayOutputStream()
+    val printStream = new PrintStream(outputStream)
+    val sorter = new StreamFrequencySorter(3, 4, 5, printStream)
+
+    sorter.processWord("hello")
+    sorter.processWord("world")
+    sorter.processWord("hello")
+
+    val output = outputStream.toString.trim
+    output must include("hello: 2")
+    output must include("world: 1")
+
+  it should "remove old words when exceeding window size" in:
+    val outputStream = new ByteArrayOutputStream()
+    val printStream = new PrintStream(outputStream)
+    val sorter = new StreamFrequencySorter(3, 4, 3, printStream)
+
+    sorter.processWord("scala")
+    sorter.processWord("java")
+    sorter.processWord("kotlin")
+    sorter.processWord("python") // This will push "scala" out of the window
+
+    val output = outputStream.toString.trim
+    output must not include "scala:"
+
+  it should "sort words by frequency and alphabetically" in:
+    val outputStream = new ByteArrayOutputStream()
+    val printStream = new PrintStream(outputStream)
+    val sorter = new StreamFrequencySorter(2, 4, 5, printStream)
+
+    sorter.processWord("apple")
+    sorter.processWord("banana")
+    sorter.processWord("apple")
+
+    val output = outputStream.toString.trim
+    output must include("apple: 2")
+    output must include("banana: 1")
+
+end StreamFrequencySorterSpec
