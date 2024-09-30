d572a218ff791a5fc555830b64205cab422ff1c3
rue <rhu1@luc.edu>
2024-09-30 11:14:46 -0500

    adding a new file to integrate previous tests. listed some more requirements for tests that should be added to meet a satisafactory coverage goal.

---
+++ src/test/scala/hellotest/temp-StackTest.scala
@@ -0,0 +1,13 @@
+// creating a new file to integrate tests over. 
+// please rename to StackTest.scala once completed 
+
+// TODO: check that functionality works as intended: 
+//     - test for the processWord and getTopWords functions individually 
+
+// TODO: check for errors with the parameters in StreamFrequencySorter: 
+//     - test for min / max parameters in minLength, minFrequency, and windowSize
+//     - test for the wordlength minimum critera has been implemented 
+//     - OPTIONAL: test to see how program responds with non-numerical inputs
+
+// TODO: check for error handling
+//     - test if sigpipe handling works as intended 
