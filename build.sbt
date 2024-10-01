a9b45d98a578c27c5f8a2081914cb5b7ef8dcf8d
Katarina Krivokapic <katarina03kk@gmail.com>
2024-10-01 11:49:41 -0500

    keeping git updated

--- build.sbt
+++ build.sbt
@@ -9,11 +9,13 @@ scalacOptions += "@.scalacOptions.txt"
 libraryDependencies += "org.log4s" %% "log4s" % "1.10.0"
 libraryDependencies += "org.slf4j" % "slf4j-simple" % "1.7.30"
 libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.4.7" % Runtime
+//libraryDependencies += "org.scalatestplus" %% "mockito-4-5" % "3.2.15.0" % Test
+libraryDependencies += "org.scalatestplus" %% "mockito-3-4" % "3.2.10.0" % Test
+
 
 libraryDependencies ++= Seq(
   "org.log4s" %% "log4s" % "1.10.0",
-  "ch.qos.logback" % "logback-classic" % "1.4.7" % Runtime,
-  "org.scalatest" %% "scalatest" % "3.2.19" % Test,
+  "org.scalatest"  %% "scalatest"  % "3.2.19"  % Test,
   "com.lihaoyi" %% "mainargs" % "0.6.3",
   "org.apache.commons" % "commons-collections4" % "4.4",
   "com.github.scopt" %% "scopt" % "4.1.0",
--- src/test/scala/hellotest/StackTest.scala
+++ src/test/scala/hellotest/StackTest.scala
@@ -6,10 +6,20 @@ import org.scalatest.prop.TableDrivenPropertyChecks._
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 import scala.util.control.NonFatal
+import org.mockito.Mockito._
+import ch.qos.logback.classic.{Level, Logger}
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.AppenderBase
+import org.log4s.getLogger
+import org.slf4j.LoggerFactory
+import scala.language.unsafeNulls
 
-class StreamFrequencySorterSpec extends AnyFlatSpec with Matchers {
-  
-  // Test the processWord function
+
+// TODO: check that functionality works as intended: 
+//     - test for the processWord and getTopWords functions individually 
+// DONE
+class StreamFrequencySorterSpec extends AnyFlatSpec with Matchers{
+  //testing out processWords function
   "StreamFrequencySorter" should "process output in order of frequency" in {
     val outputStream = new ByteArrayOutputStream()
     val printStream = new PrintStream(outputStream)
@@ -30,9 +40,9 @@ class StreamFrequencySorterSpec extends AnyFlatSpec with Matchers {
       frequency shouldEqual expectedFrequency
     }
   }
-
-  // Test the getTopWords function
+  //testing out getTopWords function
   it should "return the top words correctly" in {
+    // initializing a new set of sorters to process words into for topword testing
     val outputStream = new ByteArrayOutputStream()
     val printStream = new PrintStream(outputStream)
     val sorter = new StreamFrequencySorter(3, 4, 10, 1)
@@ -49,15 +59,22 @@ class StreamFrequencySorterSpec extends AnyFlatSpec with Matchers {
 
     topWords shouldEqual expectedTopWords
   }
-
-  // Test different parameters handling
+// TODO: check for errors with the parameters in StreamFrequencySorter: 
+//     - test for min / max parameters in minLength, minFrequency, and windowSize
+//     - test for the wordlength minimum critera has been implemented 
+//     - OPTIONAL: test to see how program responds with non-numerical inputs
   it should "handle different parameters correctly" in {
     val testCases = Table(
+      // first element displays the order of test table
+      // tl;dr: parameters | words: fed input | expectedTopWords: expected output
       ("cloudSize", "minLength", "windowSize", "minFrequency", "words", "expectedTopWords"),
       (3, 4, 5, 1, Seq("hello", "world", "hello", "scala"), Seq(("hello", 2), ("world", 1), ("scala", 1))),
-      (3, 6, 5, 1, Seq("hello", "wor", "banana"), Seq(("banana", 1))), // test minLength
-      (3, 4, 3, 1, Seq("scala", "java", "python", "kotlin"), Seq(("python", 1), ("kotlin", 1))), // test window size
-      (3, 4, 5, 2, Seq("apple", "banana", "apple", "apple"), Seq(("apple", 3))) // test minimum frequency
+      // test minLength
+      (3, 6, 5, 1, Seq("hello", "wor", "banana"), Seq(("banana", 1))),
+      // test window size
+      (3, 4, 3, 1, Seq("scala", "java", "python", "kotlin"), Seq(("python", 1), ("kotlin", 1))),
+      // test minimum frequency
+      (3, 4, 5, 2, Seq("apple", "banana", "apple", "apple"), Seq(("apple", 3)))
     )
 
     forAll(testCases) { (cloudSize, minLength, windowSize, minFrequency, words, expectedTopWords) =>
@@ -65,12 +82,75 @@ class StreamFrequencySorterSpec extends AnyFlatSpec with Matchers {
       val printStream = new PrintStream(outputStream)
       val sorter = new StreamFrequencySorter(cloudSize, minLength, windowSize, minFrequency)
 
-      words.foreach(sorter.processWord)
+      words.foreach { word =>
+      sorter.processWord(word)
+      } 
 
       val topWords = sorter.getTopWords(cloudSize)
 
       topWords shouldEqual expectedTopWords
     }
   }
-  // sigpipe handling works because the logger said it does trust me bro
+// TODO: check for error handling
+//     - test if sigpipe handling works as intended 
+  /*it should "handle SIGPIPE correctly in printWordCloud" in {
+    val outputStream = new ByteArrayOutputStream()
+    val printStream = new PrintStream(outputStream)
+    val sorter = new StreamFrequencySorter(3, 4, 5, 1)
+    var exitCalled = false
+
+    sorter.processWord("test")
+    sorter.processWord("word")
+
+    printStream.close()
+
+    try {
+      Main.printWordCloud(sorter, 10, printStream)
+    } 
+    catch {
+      case NonFatal(e) =>
+        exitCalled = true
+    }
+    exitCalled shouldBe true
+  } */
 }
+ // has the error "Values of types ch.qos.logback.classic.Level and ch.qos.logback.classic.Level | Null cannot be compared with == or !="
+ // so im commenting it out for now
+ /*
+class LogCaptureAppender extends AppenderBase[ILoggingEvent] {
+  private val events = scala.collection.mutable.ArrayBuffer.empty[ILoggingEvent]
+
+  override def append(eventObject: ILoggingEvent): Unit = {
+    events.append(eventObject)
+  }
+
+  def getEvents: Seq[ILoggingEvent] = events.toSeq
+}
+
+class LoggingTest extends AnyFlatSpec with Matchers {
+  "Main" should "log appropriate messages" in {
+    // Capture logger output
+    val logger = LoggerFactory.getLogger(org.log4s.getLogger.getClass).asInstanceOf[Logger]
+    val appender = new LogCaptureAppender()
+    appender.setContext(logger.getLoggerContext)
+    logger.addAppender(appender)
+    appender.start()
+
+    // Test the logging functionality in Main
+    Main.main(Array("--cloud-size", "5", "--length-at-least", "3"))
+
+    // Check that logs were created (at least one info log should be there)
+    val logs = appender.getEvents
+    logs should not be empty
+
+    // Check specific log message content
+    // Check specific log message content
+    val infoLogs = logs.filter(_.getLevel == Level.INFO).map(_.getFormattedMessage)
+
+    // Use contains to check for message existence without null issues
+    infoLogs.contains("program start (logging begins here)") should be(true)
+
+    appender.stop()
+  }
+}
+  */
